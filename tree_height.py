9560376b50ff308f86c77ca6c4547f249dd983e5
Edgars Lejnieks <125568758+EdgarsLejnieks@users.noreply.github.com>
2023-03-03 08:26:31 +0000

    first commit, working text input

--- tree_height.py
+++ tree_height.py
@@ -6,7 +6,13 @@ import numpy
 
 
 def compute_height(n, parents):
+    print("Debug: running compute_height")
     # Write this function
+
+    # Purely theoretically, the height of a tree corresponds to the amount
+    # of times there is an element higher than the prior, therefore:
+
+
     max_height = 0
     # Your code here
     return max_height
@@ -14,10 +20,32 @@ def compute_height(n, parents):
 
 def main():
     # implement input form keyboard and from files
+    print("I for text input, F for file input")
+    print("select input type: ")
+    inputtype = input()
+    if inputtype == "I":
+        print("Input node count: ")
+        nodecount = int(input())
+        print("Input node values: ")
+        parents = ""
+        for n in range(nodecount):
+            parents = parents + input() + " "
+        print("Parents var: " + parents)
+        # compute_height(nodecount, parents)
+        
+
+    elif inputtype == "F":
+        print("Input file path ")
+        filepath = input()
+        file = open(filepath, "r")
+        text = file.read()
+        #todo: function
     
+    # todo:
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
     
+    # todo:
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
